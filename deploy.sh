969ae8c11ce7a1b61ec0444eed48b90811b762fe
Thomas Jung <tjung@clari.com>
2022-05-28 17:31:33 -0700

    upgrade to latest version of Mail: 16

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
-rm -rf ~/Library/Mail/Bundles/GMailinator.mailbundle
-cp -R /tmp/GMailinator/ ~/Library/Mail/Bundles/
+sudo rm -rf ~/Library/Mail/Bundles/GMailinator.mailbundle
+sudo cp -R /tmp/GMailinator/ ~/Library/Mail/Bundles/
+defaults write com.apple.mail EnableBundles -bool true
 sudo spctl --add ~/Library/Mail/Bundles/GMailinator.mailbundle
